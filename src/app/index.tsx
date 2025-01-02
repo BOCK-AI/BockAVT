6b366c848d7493df56cb9e48a109bafaaacf571e
chiru-6 <chirandan14@gmail.com>
2025-01-02 22:34:14 +0530

    second commit(index.tsx a bit diff)

---
+++ src/app/index.tsx
@@ -0,0 +1,21 @@
+import {View, Text, Image} from 'react-native';
+import React from 'react';
+import { commonStyles } from '@/styles/commonStyles';
+import { splashStyles } from '@/styles/splashStyles';
+
+const Main = () => {
+    return (
+        <View style={commonStyles.container}>
+            {/* <Image
+                source={require('@/assets/images/logo_t.png')} 
+                style=(splashStyles.img)
+            /> */}
+            <Image
+                source={require('@/assets/images/logo_t.png')} 
+                style={splashStyles.img}
+            />
+        </View>
+    )
+}
+
+export default Main;
---
+++ src/utils/Helpers.tsx
@@ -0,0 +1,11 @@
+import { Href, router } from "expo-router";
+
+export const resetAndNavigate = (newPath: Href) => {
+    if (router.canGoBack()) {
+        router.dismissAll();
+    }
+    router.replace(newPath);
+}
+
+
+export { Href };
